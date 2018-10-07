1ecd3e0ddd39a2192453eaac30858c83136fbdfe
Vlad <tretyak.vladik@gmail.com>
2018-10-07 20:36:48 +0300

    Initial Commit

---
+++ PinterestFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PinterestFeed
+//
+//  Created by Влад Третьяк on 10/7/18.
+//  Copyright © 2018 Влад Третьяк. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
